6d287acde1aae9634561c8beed98aeb5f261fc03
Georgiy Savelyev <savelevgeo.post@gmail.com>
2023-05-09 00:02:38 +0300

    finish getting locales from gsheets

--- src/App.vue
+++ src/App.vue
@@ -7,9 +7,16 @@ interface localeJson {
     [key: string]: string | localeJson;
 }
 
-function nestJson(nestKey: string): localeJson {
-    
-    return { nestKey: nestKey } 
+function addNestedValues(obj: localeJson, key: string, value: string) {
+    if (key.includes('.')) {
+        const [parentKey, childKey] = key.split('.')
+
+        if (!Object.keys(obj).includes(parentKey))
+            obj[parentKey] = {}
+
+        obj[parentKey][childKey] = value
+
+    } else obj[key] = value
 }
 
 function tsvToJson(tsv: string): localeJson {
@@ -19,13 +26,11 @@ function tsvToJson(tsv: string): localeJson {
     for (let line of tsv.split('\r\n').slice(1)) {
         const [key, enValue, ruValue] = line.split('\t')
 
-        if (key.includes('.')) 
-            en[key] = nestJson(key)
-         else en[key] = enValue
-
-        console.log([key, enValue, ruValue])
+        addNestedValues(en, key, enValue)
+        addNestedValues(ru, key, ruValue)
     }
-    return en
+
+    return { en, ru }
 }
 
 const sheetUrl = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vTvdaFvqr6gxdixvsKGtL6B7YbKxTeEuV3h2P-9xelyw8Q0uorxO_Shqos8IDT-FOZ616LmRnhtCEju/pub?gid=0&single=true&output=tsv'
